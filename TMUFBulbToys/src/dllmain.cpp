2fa21d64974793b614c5a54e981fe6c5da3ea704
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2024-10-22 02:25:55 +0200

    tmuf version check

--- TMUFBulbToys/src/dllmain.cpp
+++ TMUFBulbToys/src/dllmain.cpp
@@ -8,8 +8,7 @@ BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID)
 	if (reason == DLL_PROCESS_ATTACH)
 	{
 		// Version check :3
-		//if (!strncmp(reinterpret_cast<char*>(0x9F03D5), ":33", 3))
-		if (true)
+		if (!strncmp(reinterpret_cast<char*>(0xCCD45B), ":3", 2))
 		{
 			BulbToys::SetupParams params;
 			params.instance = instance;
