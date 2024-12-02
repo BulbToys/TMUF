e7bb7ddb3808d47ae42cb71da52e5729ec8c0524
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2024-12-03 00:37:05 +0100

    fix analyser shadow not working 1st time

--- TMUFBulbToys/src/modules/analyser.cpp
+++ TMUFBulbToys/src/modules/analyser.cpp
@@ -17,13 +17,13 @@ namespace analyser
 
 	void __fastcall CGameAnalyzer_Init_(uintptr_t cga)
 	{
-		CGameAnalyzer_Init(cga);
-
 		// *(*(this + 40) + 44) - some CTrackManiaRaceAnalyzerContext shit idfk
 		auto style = Read<uintptr_t>(Read<uintptr_t>(cga + 0x28) + 0x2C);
 
 		auto grayed_char_attribs = reinterpret_cast<TMUF::CFastStringInt*>(style + 0xA8);
 		grayed_char_attribs->SetString(2, L"$s");
+
+		CGameAnalyzer_Init(cga);
 	}
 }
 
